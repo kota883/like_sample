5f78a8df47be2afc624e1f7e509fcda3d132a312
kota883 <78450427+kota883@users.noreply.github.com>
2021-06-28 22:57:02 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
